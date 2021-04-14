bc04b68c5c65fb1cb2109fcb7ff8eaebf8505590
nason <nason-ma@users.noreply.github.com>
2021-04-14 03:32:48 +0000

    Apply fixes from StyleCI

--- src/Weather.php
+++ src/Weather.php
@@ -46,7 +46,7 @@ class Weather
         return $this->getWeather($city, 'all', $format);
     }
 
-    public function getWeather($city,$type = 'base',$format = 'json')
+    public function getWeather($city, $type = 'base', $format = 'json')
     {
         // 高德地图天气查询 url
         $url = 'https://restapi.amap.com/v3/weather/weatherInfo';
